09c2cbe0ca53455f5c9fcf78abd1d18e6a81ab90
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-10-10 18:07:39 +0800

    Update Rules.php

--- src/WolfGangRule/Rules.php
+++ src/WolfGangRule/Rules.php
@@ -11,56 +11,55 @@
   *  Team: FutureDeveloperZ
   *  Link: http://github.com/FutureDeveloperZ
   *
-*/
+**/
 
-
+declare(strict_types=1);
 namespace Rules;
 
-//**PocketMine**\\
+// PocketMine
+use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
-use pocketmine\{command\ConsoleCommandSender, Server, Player, utils\TextFormat};
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
-//**Custom Plugin**\\
+// Imported or Implements
 use jojoe77777\FormAPI;
 
-class Main extends PluginBase implements Listener{
+class Rules extends PluginBase implements Listener {
 	
-	public function onEnable(){
-		$this->getServer()->getLogger()->Info("§aWolfGangUI Rules has been enabled!\n\n\n\§l§b§onSubcribe channel Wolfkid!!");
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
+    public function onEnable(){
+        $this->getLogger()->info("Â§aWolfGangUI Rules has been enabled!\n\n\n\Â§lÂ§bÂ§onSubcribe channel Wolfkid!!");
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 	
-	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
-		$player = $sender->getPlayer();
-		switch($cmd->getName()){
-			case "rules":
-			$this->mainForm($player);
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        $player = $sender->getPlayer();
+	switch($command->getName()){
+           case "rules":
+           $this->mainForm($player);
         }
         return true;
     }
 	
-	public function mainForm($player){
-		if($player instanceof Player){
-			$formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-			$form = $formapi->createSimpleForm(function (Player $event, array $data){
-				$player = $event->getPlayer();
-				if(isset($data[0])){
-					switch($data[0]){
-						}
-				}
-			});
-			$form->setTitle("§l§eRules ");
-			$form->setContent("§o§aPlease Read them Carefully!");
-			$form->addLabel("§e1. Dont Hack");
-			$form->addLabel("§e2. Dont Cheat");
-			$form->addLabel("§e3. Dont be rude to people or STAFF ");
-			$form->addLabel("§e4. Dont Team in SkyWars or Spleef");
-			$form->addLabel("§e5. Dont Curse or Talk SOS words");
-			$form->addLabel("§e6. Dont Do Bad Stuff");
-			$form->sendToPlayer($player);
-		}
+    public function mainForm($player): void {
+        $formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+	$form = $formapi->createSimpleForm(function (Player $player, $data){
+           switch ($data) {
+           }
+	});
+	$form->setTitle("Â§lÂ§eRules ");
+	$form->setContent("Â§oÂ§aPlease Read them Carefully!");
+	$form->addLabel("Â§e1. Dont Hack");
+	$form->addLabel("Â§e2. Dont Cheat");
+	$form->addLabel("Â§e3. Dont be rude to people or STAFF ");
+	$form->addLabel("Â§e4. Dont Team in SkyWars or Spleef");
+	$form->addLabel("Â§e5. Dont Curse or Talk SOS words");
+	$form->addLabel("Â§e6. Dont Do Bad Stuff");
+	$form->sendToPlayer($player);
+    }
 }
 			
